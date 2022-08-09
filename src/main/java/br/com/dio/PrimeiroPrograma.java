e1682fe96da743ab2899f148958e7ed119276388
valeriasouza28 <valeriasouza28@gmail.com>
2022-08-09 17:45:06 -0300

    first commit

---
+++ src/main/java/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,34 @@
+package br.com.dio;
+
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*vou na aba VCS do inteliJ, selecione a opção enable version control integration e selecione Git,
+        * vai de novo em VCS que a pós o repositório criado nome pode mudar para Git, e vocẽ pode selecionar a opção commit ou
+        * pressionar ctrl + K.
+        * E na parte de commit message coloque a mensagem do seu commit, e clique em Commit and Push*/
+
+        //Para refatorar o código, colocá-lo na indentação correta pressione ctrl + Alt + L
+        //Para apagar a linha usamos ctrl + Y
+
+        //Para refatora o nome da classe clique sobre ela
+        // e pressione Shift + f6
+
+        //Para colocar só o editor de texto amostra pressione Shift + F12
+
+        //Para comentar um bloco selecione o bloco e pressione ctrl + Shift + /
+
+
+        //Ou podemos selecionar o bloco de código ctrl + / para comentar as linhas
+        // desse código com // em cada linha
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+}
